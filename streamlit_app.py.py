d1cc75dd941c60cbc521437980ac2171aa116ab6
Pravachans <139697770+Pravachans@users.noreply.github.com>
2023-07-22 17:08:02 +0200

    streamlit_app.py.py

--- streamlit_app.py.py
+++ streamlit_app.py.py
@@ -6,11 +6,7 @@ import streamlit as st
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+
 
 
 
